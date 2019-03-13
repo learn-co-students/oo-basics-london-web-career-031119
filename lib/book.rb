ef53013977b475112da9159428eb191399cf61ba
Fernando <fernando_lopezc@live.com>
2019-03-13 14:51:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
-
+class Book  
+    attr_accessor :title , :author, :genre ,:page_count
+    #attr_reader :page_count
+def initialize(title)
+    @title= title  
+end
+# def page_count=(num)
+#     @page_count = num
+#   end
+ 
+#   def page_count   #i used the macro atrrbitue to replce this 
+#     @page_count
+#   end
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+
+class Shoe #blueprint for every instance of shoe 
+
+attr_accessor :brand,:color,:size,:material,:condition
+def initialize(brand)
+@brand = brand
+end
+def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+end
+end
